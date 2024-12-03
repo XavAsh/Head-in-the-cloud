e8ebe605f554d5aa13ec88e67ff513d8ae2fad31
Xavier Ashton <xavierjmashton@gmail.com>
2024-12-03 07:36:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Head in the Clouds </h1>
+        <p>
+          Welcome to the Head in the Clouds project!
+          Please enter your name bellow to get started.
+        </p>
+        <input type="text" placeholder="Enter your name" />
+        <button>Submit</button>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
